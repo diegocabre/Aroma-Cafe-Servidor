9475e976c39dada9efcf61a87012f10ea21e929e
Diego Cabre <diegocabre@gmail.com>
2024-04-16 08:29:39 -0400

    index modificado

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 const { PORT } = process.env;
-const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "https://aroma-cafe-cliente.onrender.com",
-  })
-);
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT: ${PORT}`);
