70087b51464aaa27c9d0f07df7c64e0224d4dabe
Diego Cabre <diegocabre@gmail.com>
2024-04-16 09:46:48 -0400

    cambio del login

--- src/middleware/checkLogin/index.js
+++ src/middleware/checkLogin/index.js
@@ -1,50 +1,47 @@
-const db = require('../../database/db')
-const { check, validationResult} = require('express-validator');
-const bcrypt = require('bcrypt');
-const format = require('pg-format');
-const {getCorreo}= require('../../database/querys/querys');
+const db = require("../../database/db");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const format = require("pg-format");
+const { getCorreo } = require("../../database/querys/querys");
 
-
-const validationFieldLogin =[
-	check('email')
-		.notEmpty()
-		.isEmail(),
-    check('password')
-		.notEmpty(),
-(req,res,next)=>{
-	try {
-		validationResult(req).throw();
-		return next()
-	} catch (error) {
-		return res.status(500).send({error: error.array()})
-	}
-}
-]
-const validateCredentials = async(req,res,next)=>{
-    const {email, password} = req.body;
-    try {
-		value=[email];
-        console.log(value);
-        const query = format(getCorreo,...value);
-        const { rows: [usuario], rowCount } = await db.query(query);
-        if(!usuario){
-            return res.status(404).json({ msg: "Correo invalido" })
-        }
-        const passwordEncriptada = usuario.contrasenya;
-        console.log(passwordEncriptada)
-        const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada)
-        console.log(passwordEsCorrecta)
-        if (!passwordEsCorrecta || rowCount==0){
-            return res.status(401).json({ msg: "contraseña incorrecta" })
-        }
-        else{
-            return next();
-        }
-    } catch (error) {
-        return res.status(500).send({error: error})
+const validationFieldLogin = [
+  check("email").notEmpty().isEmail(),
+  check("password").notEmpty(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-}
+    next();
+  },
+];
 
+const validateCredentials = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const value = [email];
+    const query = format(getCorreo, ...value);
+    const {
+      rows: [usuario],
+      rowCount,
+    } = await db.query(query);
+    if (!usuario || rowCount === 0) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+    const passwordEncriptada = usuario.contrasenya;
+    const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada);
+    if (!passwordEsCorrecta) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+    next();
+  } catch (error) {
+    console.error("Error:", error);
+    return res
+      .status(500)
+      .json({
+        error: "Error en el servidor. Por favor, intenta nuevamente más tarde.",
+      });
+  }
+};
 
-
-module.exports ={validateCredentials,validationFieldLogin}
+module.exports = { validateCredentials, validationFieldLogin };
